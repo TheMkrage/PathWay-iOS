680bbe3d665a43f8ff0f1368c95e864fd735613f
Matthew Krager <metoofarmer@yahoo.com>
2018-10-13 00:06:42 -0700

    waypoint types

--- PathWay/ViewController.swift
+++ PathWay/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
     @IBOutlet var addButton: UIButton!
     @IBOutlet var pathToClosest: UITextField!
     
+    let waypointTypes = ["Bathroom", "Exit", "Health Office", "Fire Alarm", "Stairs", "Elevators", "Ramps"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.startReceivingLocationChanges()
@@ -39,6 +41,13 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
         
         self.altitudeLabel.text = "20"
         self.altitudeLabel.textColor = UIColor.white
+        
+        self.pathToClosest.delegate = self
+        let inputView = UIPickerView()
+        inputView.delegate = self
+        inputView.dataSource = self
+        self.pathToClosest.inputView = inputView
+        
         self.sceneView.addSubview(self.altitudeLabel)
         
         self.setupConstraints()
@@ -99,6 +108,42 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
     }
     
     @IBAction func addPressed() {
-        
+        let alert = UIAlertController(title: "Add New Waypoint", message: "Type a name for this new waypoint", preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "Adding new Waypoint", style: .default) { (alertAction) in
+            let textField = alert.textFields![0] as UITextField
+            print(textField.text)
+            // TODO: Upload
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter your name"
+        }
+        alert.addAction(action)
+        self.present(alert, animated:true, completion: nil)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        return true
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.waypointTypes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.waypointTypes[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.pathToClosest.text = "Path to Nearest \(self.waypointTypes[row])"
+        self.view.endEditing(true)
     }
 }
